4745c5084e522aeac307a0cc6b236629d92e6c49
JPMG2 <jpmorenog22@gmail.com>
2025-10-14 21:35:21 -0300

    Refactor `Menu` and `Category` models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Traits\RecordActivity;
 use Database\Factories\CategoryFactory;
-<<<<<<< HEAD
-=======
 use Illuminate\Database\Eloquent\Attributes\Scope;
->>>>>>> 3514fa4 (Refactor `CommonQuerys` to `CommonQueries` across the codebase for improved naming consistency, update `CompanyWatcher`.)
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -17,9 +15,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 final class Category extends Model
 {
-    /** @use HasFactory<CategoryFactory> */
+    /**
+     * @use HasFactory<CategoryFactory>
+     */
     use HasFactory;
 
+    use RecordActivity;
     use SoftDeletes;
 
     protected $fillable = ['categori_code', 'categori_name', 'state_id'];
@@ -29,8 +30,6 @@ final class Category extends Model
         return $this->hasMany(Service::class);
     }
 
-<<<<<<< HEAD
-=======
     public function state()
     {
         return $this->belongsTo(State::class);
@@ -50,7 +49,6 @@ final class Category extends Model
             ->orderBy('categori_name', 'ASC');
     }
 
->>>>>>> 3514fa4 (Refactor `CommonQuerys` to `CommonQueries` across the codebase for improved naming consistency, update `CompanyWatcher`.)
     protected function casts(): array
     {
         return ['state_id' => 'integer'];
@@ -66,7 +64,7 @@ final class Category extends Model
     protected function categoriName(): Attribute
     {
         return Attribute::make(
-            set: fn ($value): string => ucfirst(mb_strtolower(mb_trim($value))),
+            set: fn ($value): string => ucwords(mb_strtolower(mb_trim($value))),
         );
     }
 }
